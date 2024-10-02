da7ea7c90c50d5b0d0a02c39473297b905b3f808
bel4onzz <bel4o_nzz@hotmail.com>
2024-10-02 13:28:25 +0200

    [homework_4_ex_1] fix title text

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="container">
       <div className="row justify-content-center align-items-center">
         <div className="col-12 col-md-8">
-          <h2 className="p-2 text-center text-secondary">Homework No.3 ex. 1</h2>
+          <h2 className="p-2 text-center text-secondary">Homework No.4 ex.1</h2>
           <div className="p-4 text-center">
             <DynamicCounter />
           </div>
