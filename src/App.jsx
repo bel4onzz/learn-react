39cbf119de4a045b674570e79529a80242761860
bel4onzz <bel4o_nzz@hotmail.com>
2024-10-02 11:14:39 +0200

    [homework_4_ex_1] init version

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import DynamicCounter from "./components/DynamicCounter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="row justify-content-center align-items-center">
+        <div className="col-12 col-md-8">
+          <h2 className="p-2 text-center text-secondary">Homework No.3 ex. 1</h2>
+          <div className="p-4 text-center">
+            <DynamicCounter />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/DynamicCounter.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+function DynamicCounter() {
+  const [counter, setCounter] = useState(0);
+
+  const handleSetCounter = (direction) => {
+    if (direction === "increment") {
+      if (counter < 10) {
+        setCounter((prevCounter) => prevCounter + 1);
+      }
+    } else {
+      if (counter > "-10") {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }
+  };
+
+  const resetCounter = () => {
+    setCounter(0);
+  };
+
+  // background color
+  const opacity = counter && Math.min(Math.abs(counter) * 0.1, 1);
+  let backgroundColor;
+  if (counter > 0) {
+    backgroundColor = `rgba(90, 122, 209, ${opacity})`; // Increment color
+  } else if (counter < 0) {
+    backgroundColor = `rgba(232, 44, 79, ${opacity})`; // Decrement color
+  } else {
+    backgroundColor = "#ffffff"; // counter = 0 color
+  }
+
+  return (
+    <div
+      className="border border-primary p-4 vh-80"
+      style={{ backgroundColor: backgroundColor }}
+    >
+      <div className="row align-items-center mb-4">
+        {/* Decrement Button */}
+        <div className="col-4 p-0 text-start">
+          <button
+            className={`btn btn-danger w-100 ${
+              counter <= -10 ? "disabled" : ""
+            }`}
+            onClick={() => handleSetCounter("decrement")}
+          >
+            Decrement
+          </button>
+        </div>
+
+        {/* Counter */}
+        <div className="col-4 p-0 text-center">
+          <p className={`fs-3 mb-1 ${Math.abs(counter) >= 5 ? "text-white" : "text-dark"}`}>{counter}</p>
+        </div>
+
+        {/* Increment Button */}
+        <div className="col-4 p-0 text-end">
+          <button
+            className={`btn btn-success w-100 ${
+              counter >= 10 ? "disabled" : ""
+            }`}
+            onClick={() => handleSetCounter("increment")}
+          >
+            Increment
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 p-0">
+          <button
+            className="btn btn-primary w-100"
+            onClick={() => resetCounter()}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DynamicCounter;
