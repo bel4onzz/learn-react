334a3ba928346e798c80dde14dfeecc8af6cc035
Aleksandar Vasilevski <bel4o_nzz@hotmail.com>
2024-10-18 14:14:53 +0200

    [homework_9_ex_1] init version

---
+++ src/layouts/Navigation.jsx
@@ -0,0 +1,22 @@
+const Navigation = () => {
+  return (
+    <>
+      <nav className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                {/* <img
+                  className="h-8 w-8"
+                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Your Company"
+                /> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+export default Navigation;
